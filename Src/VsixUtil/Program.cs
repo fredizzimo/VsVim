056c64c875c1b6e4dfd286341a736141fc1cacba
Jared Parsons <jaredpar@microsoft.com>
2014-09-03 07:57:47 -0700

    Dogfood script supports dev14

--- Src/VsixUtil/Program.cs
+++ Src/VsixUtil/Program.cs
@@ -12,6 +12,7 @@ namespace VsixUtil
         Vs2010,
         Vs2012,
         Vs2013,
+        VsDev14
     }
 
     static class Extensions
@@ -110,6 +111,8 @@ namespace VsixUtil
                     return "11";
                 case Version.Vs2013:
                     return "12";
+                case Version.VsDev14:
+                    return "14";
                 default:
                     throw new Exception("Bad Version");
             }
@@ -141,6 +144,9 @@ namespace VsixUtil
                     break;
                 case Version.Vs2013:
                     suffix = ".12.0";
+                    break;
+                case Version.VsDev14:
+                    suffix = ".14.0";
                     break;
                 default:
                     throw new Exception("Bad Version");
